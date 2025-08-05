7f05338fc16c257b391318af5d515268662a7ca8
doctorjae007 <89516364+doctorjae007@users.noreply.github.com>
2025-08-05 15:05:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/thai-speech-practice/',
+  base: '/read2/',
   plugins: [react(),tailwindcss()],
 })
